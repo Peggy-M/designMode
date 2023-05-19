cd0ad7be929629a01b8b425a068faf5c9e22e5d5
peggy <3209487776@qq.com>
2023-05-19 11:14:28 +0800

    对象浅克隆

---
+++ prototypePattern/src/main/java/com/peggy/User.java
@@ -0,0 +1,6 @@
+package com.peggy;/**
+*@Author Peggy
+* @Date 2023-05-19 11:02
+**/
+public class User {
+}
---
+++ prototypePattern/src/main/java/com/peggy/prototype/ConcretePrototypeA.java
@@ -0,0 +1,6 @@
+package com.peggy.prototype;/**
+*@Author Peggy
+* @Date 2023-05-19 10:54
+**/
+public class ConcretePrototypeA {
+}
